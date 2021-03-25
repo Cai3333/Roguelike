7d30c9de7dfe1dfd2a735f46d6f971ef6caf0de4
FuzzyGrim <fvhho661@anonaddy.me>
2021-03-25 13:32:32 +0100

    relative path

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 # add subdirectory to the path to import gamefiles
 import sys
-sys.path.insert(0, 'c:\Python\Projects\Roguelike\gamefiles')
+sys.path.insert(0, './gamefiles')
 
 # gamefiles
 import startup
